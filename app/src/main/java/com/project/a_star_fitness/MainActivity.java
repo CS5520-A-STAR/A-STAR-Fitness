5763802676987a0497e6ec755624bf2184f827b6
guangpengwu <wu.guangp@northeastern.edu>
2021-12-01 17:13:48 -0800

    MainActivity Commit

--- app/src/main/java/com/project/a_star_fitness/MainActivity.java
+++ app/src/main/java/com/project/a_star_fitness/MainActivity.java
@@ -1,16 +1,102 @@
 package com.project.a_star_fitness;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseAuth mAuth;
+    private TextView register;
+    private Button signIn;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //test
+        mAuth = FirebaseAuth.getInstance();
+        register = findViewById(R.id.register);
+        signIn = findViewById(R.id.sign_in);
+        editTextEmail = findViewById(R.id.email_address);
+        editTextPassword = findViewById(R.id.password);
+        progressBar = findViewById(R.id.progress_bar);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.register:
+                startActivity(new Intent(this, RegisterActivity.class));
+                break;
+            case R.id.sign_in:
+                userLogin();
+                break;
+        }
+    }
+
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please provide valid email!");
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password is required!");
+            editTextPassword.requestFocus();
+            return;
+        }
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    if (user.isEmailVerified()) {
+                        // redirect to user profile
+                        Toast.makeText(MainActivity.this, "We'll enter new activity", Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.GONE);
+                        startActivity(new Intent(MainActivity.this, HomeActivity.class));
+
+                    } else {
+                        user.sendEmailVerification();
+                        Toast.makeText(MainActivity.this, "Please check your email to verify your account!", Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.GONE);
+                    }
+
+                } else {
+                    Toast.makeText(MainActivity.this, "Failed to login! Please check your credentials", Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+
     }
 }
