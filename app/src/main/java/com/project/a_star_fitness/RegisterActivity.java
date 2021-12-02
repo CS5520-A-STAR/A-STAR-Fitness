31e9b6192588a4e434188359985ac0ebd02df7b2
guangpengwu <wu.guangp@northeastern.edu>
2021-12-01 17:16:10 -0800

    RegisterActivity Commit

---
+++ app/src/main/java/com/project/a_star_fitness/RegisterActivity.java
@@ -0,0 +1,109 @@
+package com.project.a_star_fitness;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth mAuth;
+    private Button registerUser;
+    private ProgressBar progressBar;
+    private EditText emailAddress;
+    private EditText passWord;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        registerUser = findViewById(R.id.register_user);
+        emailAddress = findViewById(R.id.email_address);
+        passWord = findViewById(R.id.password);
+
+        progressBar = findViewById(R.id.progress_bar);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.register_user:
+                registerUser();
+                break;
+        }
+    }
+
+    private void registerUser() {
+        String email = emailAddress.getText().toString().trim();
+        String password = passWord.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            emailAddress.setError("Email is required!");
+            emailAddress.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailAddress.setError("Please provide valid email!");
+            return;
+        }
+
+        if (password.isEmpty()) {
+            passWord.setError("Password is required!");
+            passWord.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            edu.neu.a_star_fitness.User user = new edu.neu.a_star_fitness.User(email);
+                            // add user from auth to firebase
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(RegisterActivity.this, "User has been registered successfully", Toast.LENGTH_SHORT).show();
+                                        progressBar.setVisibility(View.GONE);
+
+                                        startActivity(new Intent(RegisterActivity.this, ProfileActivity.class));
+
+                                        // if write to firebase fails
+                                    } else {
+                                        Toast.makeText(RegisterActivity.this, "Failed to register! Try again!", Toast.LENGTH_SHORT).show();
+                                        progressBar.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
+                            // if createUser with auth fails
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Failed to register! Try again!", Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+
+    }
+}
