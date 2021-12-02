5a6d5cb4c8c1b9d53510ca32b47da091d92c3977
guangpengwu <wu.guangp@northeastern.edu>
2021-12-01 17:15:18 -0800

    DaoUser Commit

---
+++ app/src/main/java/com/project/a_star_fitness/DAOUser.java
@@ -0,0 +1,34 @@
+package com.project.a_star_fitness;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import java.util.HashMap;
+
+public class DAOUser {
+
+    private DatabaseReference databaseReference;
+
+    public DAOUser() {
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        //databaseReference=db.getReference(GlobalClass.class.getSimpleName());
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+    }
+
+    public Task<Void> add(edu.neu.a_star_fitness.User user, String email) {
+        return databaseReference.child("Users").child(email).setValue(user);
+        //return databaseReference.push().setValue(user);
+    }
+
+    public Task<Void> update(String key, HashMap<String, Object> hashMap) {
+        return databaseReference.child("Users").child(key).updateChildren(hashMap);
+    }
+
+    public Query get() {
+        return databaseReference.orderByKey();
+    }
+
+
+}
